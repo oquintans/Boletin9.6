5e9ef2a27dd64a484fe2b4ac3e8c5f4b374bd736
Oscar <Oscar@Oscar-PC>
2014-11-17 23:35:00 +0100

    metodo calcular

--- src/ejercicio6/Empresa.java
+++ src/ejercicio6/Empresa.java
@@ -13,21 +13,26 @@ import javax.swing.JOptionPane;
  */
 public class Empresa {
 
-    private double sueldo;
-    private int porcent;
+    private int cont1 = 0;
+    private int cont2 = 0;
+    private int cont3 = 0;
 
-    public void pedirSueldo() {
+    public double pedirSueldo() {
+
         double s;
         do {
             String dato = JOptionPane.showInputDialog("Sueldo");
             s = Double.parseDouble(dato);
+            cont1++;
             if (s < 0) {
                 this.validarDouble();
             }
         } while (s != 0);
+        return s;
     }
 
     public double validarDouble() {
+
         double s;
         do {
             String dato = JOptionPane.showInputDialog("ERROR\nIntroduzca Sueldo");
@@ -35,4 +40,23 @@ public class Empresa {
         } while (s < 0);
         return s;
     }
+
+    public void calcular() {
+
+        double sueldo = this.pedirSueldo();
+        while (sueldo >= 1000 && sueldo <= 1750) {
+            cont2++;
+        }
+        while (sueldo < 1000) {
+            cont3++;
+        }
+
+    }
+
+    public void verSueldo() {
+        
+        
+        float porcent = (cont3 * 100)/cont1;
+    }
+
 }
