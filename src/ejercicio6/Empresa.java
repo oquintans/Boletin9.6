0c1683e601960aa04491bb88036ab1b718c21663
Oscar <Oscar@Oscar-PC>
2014-11-17 21:06:44 +0100

    metodo validarDouble

---
+++ src/ejercicio6/Empresa.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio6;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Empresa {
+
+    private double sueldo;
+    private int porcent;
+
+    public void pedirSueldo() {
+        
+    }
+
+    public double validarDouble() {
+        double s;
+        do {
+            String dato = JOptionPane.showInputDialog("ERROR\nIntroduzca Sueldo");
+            s = Double.parseDouble(dato);
+        } while (s < 0);
+        return s;
+    }
+}
